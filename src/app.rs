7d109c8703bb2e1fca419d35ecc0c03d4f98e7a1
Alexander Kolov <me@alexkolov.com>
2025-03-14 18:59:50 +0900

    Connection queue

--- src/app.rs
+++ src/app.rs
@@ -6,49 +6,91 @@ use axum::{
     routing::get,
     Router,
 };
-use std::collections::HashMap;
-use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
-use tokio::sync::Mutex;
-use tower_http::cors::{Any, CorsLayer};
-use tower_http::{compression::CompressionLayer, trace::TraceLayer};
-use tower_http::timeout::TimeoutLayer;
+use std::{collections::HashMap, sync::Arc, time::Duration};
+use tokio::{
+    sync::{
+        mpsc::{self, Sender},
+        oneshot, Mutex,
+    },
+    task,
+};
+use tower_http::{
+    compression::CompressionLayer,
+    cors::{Any, CorsLayer},
+    timeout::TimeoutLayer,
+    trace::TraceLayer,
+};
 
 use crate::interfaces::{AppError, DbDefaults, DbPath, QueryParams, QueryResponse};
-use crate::query;
 use crate::state::AppState;
-use crate::websocket;
+use crate::{query, websocket};
+
+type QueryResultSender = oneshot::Sender<Result<QueryResponse, AppError>>;
+type QueryQueueItem = (QueryParams, QueryResultSender);
+type QueryReceiver = mpsc::Receiver<QueryQueueItem>;
+type SharedQueryReceiver = Arc<Mutex<QueryReceiver>>;
+
+#[derive(Clone)]
+pub struct QueueState {
+    sender: Sender<(
+        QueryParams,
+        tokio::sync::oneshot::Sender<Result<QueryResponse, AppError>>,
+    )>,
+}
 
 #[axum::debug_handler]
 async fn handle_get(
-    State(state): State<Arc<AppState>>,
+    State((queue_state, app_state)): State<(QueueState, Arc<AppState>)>,
     ws: Result<WebSocketUpgrade, WebSocketUpgradeRejection>,
     Query(params): Query<QueryParams>,
 ) -> Result<QueryResponse, AppError> {
     if let Ok(ws) = ws {
-        // WebSocket upgrade
         Ok(QueryResponse::Response(
-            ws.on_upgrade(|socket| websocket::handle(socket, state)),
+            ws.on_upgrade(|socket| websocket::handle(socket, app_state)),
         ))
     }
     else {
-        // HTTP request
-        query::with_db_retry(&state, params, |state, params| Box::pin(query::handle(state, params))).await
+        let (tx, rx) = tokio::sync::oneshot::channel();
+        queue_state.sender.send((params, tx)).await.unwrap();
+        rx.await.unwrap()
     }
 }
 
 #[axum::debug_handler]
 async fn handle_post(
-    State(state): State<Arc<AppState>>,
+    State((queue_state, _app_state)): State<(QueueState, Arc<AppState>)>,
     Json(params): Json<QueryParams>,
 ) -> Result<QueryResponse, AppError> {
-    let thread_id = thread::current().id();
-    tracing::info!("Handling request on thread {:?}", thread_id);
-    query::with_db_retry(&state, params, |state, params| Box::pin(query::handle(state, params))).await
+    let (tx, rx) = oneshot::channel();
+    queue_state.sender.send((params, tx)).await.unwrap();
+    rx.await.unwrap()
 }
 
-pub async fn app(defaults: DbDefaults, db_paths: Vec<DbPath>, timeout: u32) -> Result<Router> {
+async fn process_queue(state: Arc<AppState>, receiver: SharedQueryReceiver) {
+    loop {
+        let msg = {
+            let mut receiver_guard = receiver.lock().await;
+            receiver_guard.recv().await
+        };
+
+        match msg {
+            Some((params, response_tx)) => {
+                let res =
+                    query::with_db_retry(&state, params, |state, params| Box::pin(query::handle(state, params))).await;
+                let _ = response_tx.send(res);
+            }
+            None => break,
+        }
+    }
+}
+
+pub async fn app(
+    defaults: DbDefaults,
+    db_paths: Vec<DbPath>,
+    timeout: u32,
+    num_processors: usize,
+    parallelism: usize,
+) -> Result<Router> {
     let app_state = Arc::new(AppState {
         defaults,
         paths: db_paths.into_iter().map(|db| (db.id.clone(), db)).collect(),
@@ -56,20 +98,34 @@ pub async fn app(defaults: DbDefaults, db_paths: Vec<DbPath>, timeout: u32) -> R
     });
 
     tracing::info!("Server timeout: {}", timeout);
+    tracing::info!("Server parallelism: {}", num_processors);
     tracing::info!("Loaded paths: {:?}", app_state.paths);
+
+    let (tx, rx): (mpsc::Sender<QueryQueueItem>, QueryReceiver) = mpsc::channel(queue_length);
+    let rx: SharedQueryReceiver = Arc::new(Mutex::new(rx));
+
+    for _ in 0..parallelism {
+        let processor_state = app_state.clone();
+        let rx_clone = Arc::clone(&rx);
+
+        task::spawn(async move {
+            process_queue(processor_state, rx_clone).await;
+        });
+    }
+
+    let queue_state = QueueState { sender: tx };
 
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods([Method::OPTIONS, Method::POST, Method::GET])
         .allow_headers(Any)
-        .max_age(Duration::from_secs(60 * 60 * 24));
+        .max_age(Duration::from_secs(86400));
 
     Ok(Router::new()
         .route("/", get(handle_get).post(handle_post))
-        .with_state(app_state)
+        .with_state((queue_state, app_state))
         .layer(cors)
         .layer(CompressionLayer::new())
         .layer(TraceLayer::new_for_http())
-        .layer(TimeoutLayer::new(Duration::from_secs(timeout.into())))
-    )
+        .layer(TimeoutLayer::new(Duration::from_secs(timeout.into()))))
 }
