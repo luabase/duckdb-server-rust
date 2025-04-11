e88a59ee7fd7165746d2edb73c9b608e1fc1c576
Alexander Kolov <me@alexkolov.com>
2025-04-12 00:38:24 +0900

    Implement reflection and healthcheck

---
+++ src/grpc_health.rs
@@ -0,0 +1,24 @@
+pub use grpc_health_v1::health_server::{Health, HealthServer};
+pub use grpc_health_v1::{HealthCheckRequest, HealthCheckResponse};
+use grpc_health_v1::health_check_response::ServingStatus;
+use tonic::{Request, Response, Status};
+
+pub mod grpc_health_v1 {
+    tonic::include_proto!("grpc.health.v1");
+    pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("health_descriptor");
+}
+
+#[derive(Default)]
+pub struct HealthCheckService {}
+
+#[tonic::async_trait]
+impl Health for HealthCheckService {
+    async fn check(&self, request: Request<HealthCheckRequest>) -> Result<Response<HealthCheckResponse>, Status> {
+        let service_name = request.into_inner().service;
+        println!("HealthCheck requested for service: {}", service_name);
+
+        Ok(tonic::Response::new(HealthCheckResponse {
+            status: ServingStatus::Serving as i32,
+        }))
+    }
+}
