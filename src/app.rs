11cef5da7b6ea8d4387b1b187eb0e665c067873f
Alexander Kolov <me@alexkolov.com>
2025-04-17 13:34:22 +0900

    Version layer

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use axum::{
     extract::{Query, State},
-    http::{HeaderValue, Method},
+    http::{header::HeaderName, HeaderValue, Method},
     response::Json,
     routing::get,
     Router,
@@ -11,14 +11,16 @@ use tower::ServiceBuilder;
 use tower_http::{
     compression::CompressionLayer,
     cors::{Any, CorsLayer},
+    set_header::SetResponseHeaderLayer,
     timeout::TimeoutLayer,
     trace::TraceLayer,
 };
 
-use crate::hostname::HostnameLayer;
+use crate::constants::FULL_VERSION;
 use crate::interfaces::{AppError, QueryParams, QueryResponse};
 use crate::query;
 use crate::state::AppState;
+
 
 #[axum::debug_handler]
 async fn handle_get(
@@ -50,6 +52,10 @@ async fn readiness_probe() -> &'static str {
     "OK"
 }
 
+async fn version_handler() -> &'static str {
+    &FULL_VERSION
+}
+
 pub async fn app(app_state: Arc<AppState>, timeout: u32) -> Result<Router> {
     let cors = CorsLayer::new()
         .allow_origin(Any)
@@ -58,17 +64,24 @@ pub async fn app(app_state: Arc<AppState>, timeout: u32) -> Result<Router> {
         .max_age(Duration::from_secs(86400));
 
     let hostname = hostname::get()?.into_string().unwrap_or_else(|_| "unknown".into());
-    let hostname_layer = HostnameLayer {
-        hostname: HeaderValue::from_str(&hostname)?,
-    };
+    let header_layer = ServiceBuilder::new()
+        .layer(SetResponseHeaderLayer::if_not_present(
+            HeaderName::from_static("X-Backend-Hostname"),
+            HeaderValue::from_str(&hostname)?,
+        ))
+        .layer(SetResponseHeaderLayer::if_not_present(
+            HeaderName::from_static("X-Server-Version"),
+            HeaderValue::from_static(&FULL_VERSION),
+        ));
 
     Ok(Router::new()
         .route("/", get(readiness_probe))
         .route("/query", get(handle_get).post(handle_post))
         .route("/query/", get(handle_get).post(handle_post))
         .route("/healthz", get(readiness_probe))
+        .route("/version", get(version_handler))
         .with_state(app_state)
-        .layer(ServiceBuilder::new().layer(hostname_layer))
+        .layer(header_layer)
         .layer(cors)
         .layer(CompressionLayer::new())
         .layer(TraceLayer::new_for_http())
