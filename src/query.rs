5a947e7af9a733c9abe156486d12c05b77879345
Alexander Kolov <me@alexkolov.com>
2025-08-19 00:09:47 +0900

    Remove pointer error handling

--- src/query.rs
+++ src/query.rs
@@ -33,8 +33,7 @@ where
                 if let Some(duckdb::Error::DuckDBFailure(_, _)) = err.downcast_ref::<duckdb::Error>() {
                     if err_str.contains("stale file handle") 
                     || err_str.contains("write-write conflict") 
-                    || err_str.contains("database has been invalidated")
-                    || err_str.contains("failed to load metadata pointer") {
+                    || err_str.contains("database has been invalidated") {
                         if attempt <= max_retries {
                             let delay = if attempt == 1 {
                                 Duration::from_secs(0)
