6ea80b4e40f6a91b66529da9934a67b405d39306
Alexander Kolov <me@alexkolov.com>
2025-08-18 20:04:39 +0900

    Reconnect db when invalidated

--- src/query.rs
+++ src/query.rs
@@ -31,7 +31,9 @@ where
                 }
                 
                 if let Some(duckdb::Error::DuckDBFailure(_, _)) = err.downcast_ref::<duckdb::Error>() {
-                    if err_str.contains("stale file handle") || err_str.contains("write-write conflict") {
+                    if err_str.contains("stale file handle") 
+                    || err_str.contains("write-write conflict") 
+                    || err_str.contains("database has been invalidated") {
                         if attempt <= max_retries {
                             let delay = if attempt == 1 {
                                 Duration::from_secs(0)
